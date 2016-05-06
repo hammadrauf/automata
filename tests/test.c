b27d29709956b4f2ae200a37c841e9d0a291af6b
Colin Atkinson <colin.william.atkinson@gmail.com>
2016-05-06 09:35:48 -0400

    Switch to a newer version of minunit

--- tests/test.c
+++ tests/test.c
@@ -4,21 +4,9 @@
 #include "minunit.h"
 #include "test_example1.h"
 
-int tests_run = 0;
-
-static char *all_tests() {
-    mu_run_test(example1);
-    return 0;
-}
-
 int main(int argc, char **argv) {
-    char *result = all_tests();
-    if (result != 0) {
-        printf("%s\n", result);
-    } else {
-        printf("ALL TESTS PASSED\n");
-    }
-    printf("Tests run: %d\n", tests_run);
+    MU_RUN_SUITE(example_1);
+    MU_REPORT();
 
-    return result != 0;
+    return 0;
 }
--- tests/test_example1.h
+++ tests/test_example1.h
@@ -6,6 +6,71 @@
 #include <stdio.h>
 #include "minunit.h"
 
-char *example1();
+MU_TEST(example_1_a) {
+    config_t cfg;
+    config_init(&cfg);
+
+    if(!config_read_file(&cfg, "../examples/example1.aut")) {
+        config_destroy(&cfg);
+        mu_fail("Could not read example1.aut");
+    }
+
+    bool acc = automata_is_accepted(&cfg, "q_0", "a");
+
+    config_destroy(&cfg);
+    mu_assert(acc, "'a' not accepted as input by example 1");
+}
+
+MU_TEST(example_1_b) {
+    config_t cfg;
+    config_init(&cfg);
+
+    if(!config_read_file(&cfg, "../examples/example1.aut")) {
+        config_destroy(&cfg);
+        mu_fail("Could not read example1.aut");
+    }
+
+    bool acc = automata_is_accepted(&cfg, "q_0", "b");
+
+    config_destroy(&cfg);
+    mu_assert(acc, "'b' not accepted as input by example 1");
+}
+
+MU_TEST(example_1_blank) {
+    config_t cfg;
+    config_init(&cfg);
+
+    if(!config_read_file(&cfg, "../examples/example1.aut")) {
+        config_destroy(&cfg);
+        mu_fail("Could not read example1.aut");
+    }
+
+    bool acc = automata_is_accepted(&cfg, "q_0", "");
+
+    config_destroy(&cfg);
+    mu_assert(!acc, "Empty string accepted as input by example 1");
+}
+
+MU_TEST(example_1_longer) {
+    config_t cfg;
+    config_init(&cfg);
+
+    if(!config_read_file(&cfg, "../examples/example1.aut")) {
+        config_destroy(&cfg);
+        mu_fail("Could not read example1.aut");
+    }
+
+    bool acc = automata_is_accepted(&cfg, "q_0", "aabababababababababa");
+
+    config_destroy(&cfg);
+    mu_assert(acc, "'aabababababababababa' not accepted as input by example 1");
+}
+
+MU_TEST_SUITE(example_1) {
+    MU_RUN_TEST(example_1_a);
+    MU_RUN_TEST(example_1_b);
+    MU_RUN_TEST(example_1_blank);
+    MU_RUN_TEST(example_1_longer);
+}
 
 #endif /* TEST_EX_1_H */
